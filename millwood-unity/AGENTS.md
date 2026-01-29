d2ffc6b8fa295024a5d1bb01e25c7953257a2991
Corbin Duncan <cpduncan@duncanprime.com>
2026-01-28 19:52:16 -0500

    Renders mesh on RaycastHit
    need to optimize the Mesh GameObject position updates, so they properly update as opposed to creating new ones and deleting old ones

--- millwood-unity/AGENTS.md
+++ millwood-unity/AGENTS.md
@@ -2,7 +2,7 @@
 - Project name: millwood-unity
 - Unity version: Unity 6000.2.12f1
 - Active game object:
-  - Name: Test Camera
+  - Name: Main Camera
   - Tag: MainCamera
   - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- millwood-unity/Assets/Scripts/RayCamController.cs
+++ millwood-unity/Assets/Scripts/RayCamController.cs
@@ -1,32 +1,22 @@
 using NUnit.Framework.Constraints;
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using static UnityEngine.UI.Image;
 
-public class renderingtest1 : MonoBehaviour
+public class RayCamController : MonoBehaviour
 {
 
-    [SerializeField] Material mat;
-
-    private Vector3[] verticies = new Vector3[4];
-    private Vector2[] uv = new Vector2[4];
-    private int[] triangles = new int[6];
-
-    private GameObject meshObject;
-    private Mesh mesh;
-
-    //Transform camCenter;
-
-    //void Start()
-    //{
-    //    camCenter = GameObject.Find("center viewpoint").transform;
-    //}
-
-    [Header("Raycast Origin")]
-    [SerializeField] public Transform rayOrigin;
+    [Header("Entity \"RayCam\" parameters")]
+    [SerializeField] private int gridWidth;
+    [SerializeField] private int gridHeight;
 
     private LayerMask layerMask;
+
+    private ContactMesh contactMesh;
+
+    [SerializeField] Transform contactMeshTransform;
 
     private void Awake()
     {
@@ -35,10 +25,45 @@ public class renderingtest1 : MonoBehaviour
 
     private void Start()
     {
-        verticies[0] = new Vector3(0, 0, 0);
-        verticies[1] = new Vector3(0, 1, 0);
-        verticies[2] = new Vector3(1, 1, 0);
-        verticies[3] = new Vector3(1, 0, 0);
+        contactMesh = new();
+    }
+    void FixedUpdate()
+    {
+
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, fwd, out hit, 10f, layerMask))
+        {
+
+            Renderer renderer = hit.transform.gameObject.GetComponent<Renderer>();
+            contactMesh.getObject().GetComponent<MeshRenderer>().material = renderer.material;
+
+            contactMesh.updateTransform(hit.point, transform.rotation);
+
+        } else {
+            print("NoHit");
+        }
+
+    }
+
+}
+
+public class ContactMesh
+{
+    private Vector3[] vertices = new Vector3[4];
+    private Vector2[] uv = new Vector2[4];
+    private int[] triangles = new int[6];
+
+    private GameObject meshObject;
+    private Mesh mesh;
+
+    public ContactMesh() {
+
+        vertices[0] = new Vector3(-.2f, -.2f, 0);
+        vertices[1] = new Vector3(-.2f, .2f, 0);
+        vertices[2] = new Vector3(.2f, .2f, 0);
+        vertices[3] = new Vector3(.2f, -.2f, 0);
 
         triangles[0] = 0;
         triangles[1] = 1;
@@ -53,33 +78,36 @@ public class renderingtest1 : MonoBehaviour
         uv[2] = new Vector2(1, 1);
         uv[3] = new Vector2(1, 0);
 
+        createMesh();
+    }
+
+    public void updateTransform(Vector3 transform, Quaternion rotation)
+    {
+        vertices[0] = transform + rotation * new Vector3(-.2f, -.2f, 0);
+        vertices[1] = transform + rotation * new Vector3(-.2f, .2f, 0);
+        vertices[2] = transform + rotation * new Vector3(.2f, .2f, 0);
+        vertices[3] = transform + rotation * new Vector3(.2f, -.2f, 0);
+        Object.Destroy(meshObject);
+        createMesh();
+    }
+
+    public void createMesh()
+    {
         mesh = new Mesh();
         mesh.name = "Custom mesh";
 
         meshObject = new GameObject("Mesh object", typeof(MeshRenderer), typeof(MeshFilter));
 
         meshObject.GetComponent<MeshFilter>().mesh = mesh;
-        meshObject.GetComponent<MeshRenderer>().material = mat;
 
-        mesh.vertices = verticies;
+        mesh.vertices = vertices;
         mesh.uv = uv;
         mesh.triangles = triangles;
     }
-    void FixedUpdate()
+
+    public GameObject getObject()
     {
-
-        Vector3 fwd = transform.TransformDirection(Vector3.forward);
-
-        RaycastHit hit;
-        if (Physics.Raycast(rayOrigin.position, fwd, out hit, 10f, layerMask))
-        {
-            Renderer renderer = hit.transform.gameObject.GetComponent<Renderer>();
-            meshObject.GetComponent<MeshRenderer>().material = renderer.material;
-            //print(renderer.material.name);
-        } else {
-            print("NoHit");
-        }
-
+        return meshObject;
     }
-
+        
 }
